2ff145ce1a0ae3693471bf0b286f23e2674db9d3
BubbleNeumann <bubbleneumann@gmail.com>
2022-09-21 11:51:10 +0400

    day 1 part 2, universal solution for both parts

--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -1,22 +1,37 @@
 #![allow(dead_code)]
 // use itertools::Itertools;
 
-
-fn part1() {
+fn one_per_line() -> Vec<i32> {
     const FILE_PATH: &str = "./data/day1.input";
     let contents = std::fs::read_to_string(FILE_PATH)
         .expect(&format!("can't read {FILE_PATH} file"));
 
-    let mut prev = -1;
-    let mut result = -1;
-
+    let mut res = Vec::new();
     for val in contents.trim().split("\n") {
-        let depth = val.parse::<i32>().unwrap();
-        result += (depth > prev) as i32;
-        prev = depth;
+        res.push(val.parse::<i32>().unwrap());
     }
 
-    println!("{result}");
+    res
 }
 
-fn main() { }
+fn count_increasing_seq(vec: Vec<i32>, seq_len: usize) -> i32 {
+    let mut res: i32 = 0;
+    let mut prev_sum = 0;
+    let mut cur_sum;
+
+    for i in 0..vec.len() - seq_len {
+        cur_sum = 0;
+        for j in 0..seq_len {
+            cur_sum += vec[i+j];
+        }
+        res += (cur_sum > prev_sum) as i32;
+        prev_sum = cur_sum;
+    }
+
+    res
+}
+
+fn main() {
+    println!("{}", count_increasing_seq(one_per_line(), 1)); // part 1
+    println!("{}", count_increasing_seq(one_per_line(), 3)); // part 2
+}
