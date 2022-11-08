b5a9eba9e7f6f00a9530fae1628a642dde37f108
BubbleNeumann <bubbleneumann@gmail.com>
2022-11-08 23:25:08 +0400

    day 2 trimmed to 1 func

--- 2021/src/day2.rs
+++ 2021/src/day2.rs
@@ -1,16 +1,14 @@
-fn parse_input() -> i32 {
+fn parse(aim_needed: bool) -> i32 {
     const FILE_PATH: &str = "./data/day2.input";
     let contents =
         std::fs::read_to_string(FILE_PATH).expect(&format!("can't read {FILE_PATH} file"));
 
     let mut forth = 0;
     let mut depth = 0;
-
+    let mut aim = 0;
 
     for string in contents.trim().split("\n") {
-
         let mut str_val = "";
-
         for val in string.trim().split_whitespace() {
             if str_val == "" {
                 str_val = val;
@@ -21,22 +19,23 @@ fn parse_input() -> i32 {
 
             if str_val == "forward" {
                 forth += num_val;
+                depth += aim * num_val * (aim_needed as i32);
             } else if str_val == "up" {
-                depth -= num_val;
+                depth -= num_val * (!aim_needed as i32);
+                aim -= num_val;
             } else {
-                depth += num_val;
+                depth += num_val * (!aim_needed as i32);
+                aim += num_val;
             }
 
             str_val = "";
         }
     }
 
-    // (forth, depth)
     forth * depth
 }
 
-
 fn main() {
-    println!("{}", parse_input());
-
+    println!("{}", parse(false));
+    println!("{}", parse(true));
 }
