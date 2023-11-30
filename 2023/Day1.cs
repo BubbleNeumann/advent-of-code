93f99e50256221dbfd3e14a33a1d501dc940418a
BubbleNeumann <bubbleneumann@gmail.com>
2023-12-01 00:21:01 +0400

    prep structure

---
+++ 2023/Day1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2023
+{
+    public class Day1
+    {
+        public static string Solve(string filename)
+        {
+            string a = File.ReadAllText(filename);
+            return a;
+        }
+    }
+}
---
+++ 2023/Program.cs
@@ -0,0 +1,3 @@
+﻿using _2023;
+
+Console.WriteLine(Day1.Solve("data/day1.txt"));
