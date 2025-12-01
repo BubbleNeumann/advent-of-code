bd7e7147af93348398c0d73a6ac6bcbb9918acd9
dana <bubbleneumann@gmail.com>
2025-12-01 23:04:04 +0300

    part 2 god forgive me

--- 2025/day1.c
+++ 2025/day1.c
@@ -25,7 +25,7 @@ void part1(const char* filename) // solved
     printf("(1) done=%d\n", cnt);
 }
 
-void part2(const char* filename)
+void part2(const char* filename) // solved
 {
     FILE* file = fopen(filename, "r");
     int dial_ptr = INIT_DIAL_PTR;
@@ -34,19 +34,14 @@ void part2(const char* filename)
 
     while ((c = fgetc(file)) != EOF)
     {
-        dir = c == 'L' ? 0 : 1; // L or R
+        dir = c == 'L' ? -1 : 1; // L or R
         r = 0;
         while ((c=fgetc(file)) - 13) r = r * 10 + c - '0';
-        int prev_dial_ptr = dial_ptr;
-        dial_ptr += dir ? r : -r;
-
-        int diff = dial_ptr - prev_dial_ptr;
-
-        // correction
-        // dial_ptr += 100 * (dial_ptr < 0) - 100 * (dial_ptr > 200);
-
-        cnt += ((dial_ptr % 100) == 0) || diff / 100 == 1 || dial_ptr * prev_dial_ptr < 0;
-
+        for (int i = 0; i < abs(r); ++i)
+        {
+            dial_ptr += dir;
+            cnt += dial_ptr % 100 == 0;
+        }
         fgetc(file); // read newline
     }
     fclose(file);
@@ -57,7 +52,7 @@ int main()
 {
     // part1("input/day1-test.txt");
     // part1("input/day1.txt");
-    part2("input/day1-test.txt");
-    // part2("input/day1.txt");
+    // part2("input/day1-test.txt");
+    part2("input/day1.txt");
     return 0;
 }
